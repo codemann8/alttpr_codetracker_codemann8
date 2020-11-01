3ab853d50273b1219560f71b3170158635c2f590
kaesden <brian.hull@me.com>
2020-11-01 11:29:53 -0800

    dam and TT attic racemode
    added racemode check for dam and TT attic. do not autotrack these items in racemode.

--- scripts/auto/itemupdates.lua
+++ scripts/auto/itemupdates.lua
@@ -165,9 +165,11 @@ function updateAga1(segment)
 end
 
 function updateDam(segment)
-    local item = Tracker:FindObjectForCode("dam")
-    local value = ReadU8(segment, 0x7ef2bb)
-    item.Active = value & 0x20 > 0
+	if OBJ_RACEMODE.CurrentStage == 0 then
+		local item = Tracker:FindObjectForCode("dam")
+		local value = ReadU8(segment, 0x7ef2bb)
+		item.Active = value & 0x20 > 0
+	end
 end
 
 function updateHealth(segment)
