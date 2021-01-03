bac384d25baa5cbeea58bd0e3f543b956d4c21b4
codemann8 <codemann8@gmail.com>
2021-01-03 02:23:18 -0600

    Added Hybrid Glitch logic

--- scripts/custom/modes/glitchmode.lua
+++ scripts/custom/modes/glitchmode.lua
@@ -7,7 +7,7 @@ function GlitchMode:init(isAlt)
     self:initSuffix(isAlt)
     self:initCode()
 
-    self:setCount(3)
+    self:setCount(4)
     self:setState(0)
 end
 
@@ -16,6 +16,8 @@ function GlitchMode:updateIcon()
         self.ItemInstance.Icon = ImageReference:FromPackRelativePath("images/mode_glitches_none" .. self.suffix .. ".png")
     elseif self:getState() == 1 then
         self.ItemInstance.Icon = ImageReference:FromPackRelativePath("images/mode_glitches_ow" .. self.suffix .. ".png")
+    elseif self:getState() == 2 then
+        self.ItemInstance.Icon = ImageReference:FromPackRelativePath("images/mode_glitches_hybrid" .. self.suffix .. ".png")
     else
         self.ItemInstance.Icon = ImageReference:FromPackRelativePath("images/mode_glitches_major" .. self.suffix .. ".png")
     end
