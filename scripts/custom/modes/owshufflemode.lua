6433b9e3f6261af7b763e1f9a761614242d35ee9
codemann8 <codemann8@gmail.com>
2021-04-25 20:17:48 -0500

    Added Mode for OW Shuffle

---
+++ scripts/custom/modes/owshufflemode.lua
@@ -0,0 +1,20 @@
+OverworldShuffleMode = SurrogateItem:extend()
+
+function OverworldShuffleMode:init(isAlt)
+    self.baseCode = "ow_shuffle"
+    self.label = "Overworld Shuffle"
+
+    self:initSuffix(isAlt)
+    self:initCode()
+
+    self:setCount(2)
+    self:setState(0)
+end
+
+function OverworldShuffleMode:updateIcon()
+    if self:getState() == 0 then
+        self.ItemInstance.Icon = ImageReference:FromPackRelativePath("images/mode_ow_shuffle_off" .. self.suffix .. ".png")
+    elseif self:getState() == 1 then
+        self.ItemInstance.Icon = ImageReference:FromPackRelativePath("images/mode_ow_shuffle_on" .. self.suffix .. ".png")
+    end
+end
