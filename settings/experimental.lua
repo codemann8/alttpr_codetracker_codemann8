970683631b3afe317df69cbb13fd12a32276d4e1
codemann8 <codemann8@gmail.com>
2022-03-25 09:12:36 -0500

    Deleted unused settings files

--- settings/experimental.lua
+++
@@ -1,5 +0,0 @@
-------------------------------------------------------------------
--- Configuration options for scripted systems in this pack
-------------------------------------------------------------------
-EXPERIMENTAL_ENABLE_DYNAMIC_REQUIREMENTS = false
-EXPERIMENTAL_INVERTED_NEW_LOGIC = false
--- settings/settings.lua
+++
@@ -1,7 +0,0 @@
-------------------------------------------------------------------
--- Configuration options for scripted systems in this pack
-------------------------------------------------------------------
-AUTOTRACKER_ENABLE_AUTOPIN_CURRENT_DUNGEON = false
-LAYOUT_ENABLE_ALTERNATE_DUNGEON_VIEW = false
-LAYOUT_USE_THIN_HORIZONTAL_PANE = false
-LAYOUT_ROOM_SLOT_METHOD = "exist"
